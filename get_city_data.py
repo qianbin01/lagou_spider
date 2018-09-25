5b46a57ad78e479799d716097a260867850efd3c
qb <qianbin@mike-mary.onaliyun.com>
2018-09-25 13:10:05 +0800

    更新全国地铁数据和商圈数据

--- get_city_data.py
+++ get_city_data.py
@@ -1,28 +1,59 @@
 import requests
+import pymongo
+import config
 
-city_with_subway_url = 'http://map.baidu.com/?qt=subwayscity'
-subway_detail_url = 'http://map.baidu.com/?qt=bsi&c={}'
-r = requests.get(city_with_subway_url)
-data_list = []
-subway_names = []
-for item in r.json().get('subways_city').get('cities'):
-    if item['code'] < 10000:
-        url = subway_detail_url.format(item['code'])
-        r = requests.get(url)
-        doc = {
-            'cityName': item['cn_name'],
-            'subWayList': []
-        }
-        for subway in r.json().get('content'):
-            if subway['line_name'].split('(')[0] not in subway_names:
-                subway_names.append(subway['line_name'].split('(')[0])
-            subway_doc = {
-                'lineName': subway['line_name'].split('(')[0],
-                'stops': []
+client = pymongo.MongoClient(host=config.MONGO_HOST, port=config.MONGO_PORT)
+db = client[config.MONGO_DB]
+db.authenticate(config.MONGO_AUTH_NAME, config.MONGO_AUTH_PASSWORD)
+city_data = db['city_data']
+subways_data = db['subways_data']
+
+
+def get_subway_data():
+    city_with_subway_url = 'http://map.baidu.com/?qt=subwayscity'
+    subway_detail_url = 'http://map.baidu.com/?qt=bsi&c={}'
+    r = requests.get(city_with_subway_url)
+    subway_names = []
+    for item in r.json().get('subways_city').get('cities'):
+        if item['code'] < 10000:
+            url = subway_detail_url.format(item['code'])
+            r = requests.get(url)
+            doc = {
+                'cityName': item['cn_name'],
+                'subWayList': []
             }
-            for stop in subway['stops']:
-                subway_doc['stops'].append(stop['name'])
-            doc['subWayList'].append(subway_doc)
-        subway_names = []
-        data_list.append(doc)
-print(data_list)
+            for subway in r.json().get('content'):
+                if subway['line_name'].split('(')[0] not in subway_names:
+                    subway_names.append(subway['line_name'].split('(')[0])
+                subway_doc = {
+                    'lineName': subway['line_name'].split('(')[0],
+                    'stops': []
+                }
+                for stop in subway['stops']:
+                    subway_doc['stops'].append(stop['name'])
+                doc['subWayList'].append(subway_doc)
+                find_data = subways_data.find_one(
+                    {'cityName': doc.get('cityName')})
+                if not find_data:  # 查重后插入数据库
+                    subways_data.insert(doc)
+            subway_names = []
+            print(doc)
+
+
+def combine_data():
+    import json
+    with open('city_data.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        cities = data.get('data')
+        for city in cities:
+            for area in city['cities']:
+                print(area)
+                find_data = city_data.find_one(
+                    {'name': area.get('name')})
+                if not find_data:  # 查重后插入数据库
+                    city_data.insert(area)
+
+
+if __name__ == '__main__':
+    combine_data()
+    get_subway_data()
